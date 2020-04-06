7e136b1e89f4642c38008f5e43a072989225faf3
Benjamin Leray <b.leray35@gmail.com>
2020-04-06 16:44:39 +0200

    fix seed and loop moviereview and bookmark

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,23 @@ puts "Creating movies"
 Movie.create!( title:'La Haine' , year:'1995', director:'Mathieu Kassovitz',poster_path:'https://image.tmdb.org/t/p/w1280/3nVoVV0RmweOgDjebtI8CFv13rf.jpg', synopsis:"Trois copains d'une banlieue ordinaire traînent leur ennui et leur jeunesse qui se perd. Ils vont vivre la journée la plus importante de leur vie après une nuit d'émeutes provoquée par le passage à tabac d'Abdel Ichah par un inspecteur de police lors d'un interrogatoire." )
 
 puts "Creating bookmarks"
-Bookmark.create!(user: User.first , moviehub : Movie.first)
-Bookmark.create!(user: User.last , moviehub : Movie.first)
+5.times do
+  Bookmark.create!(user: User.first , movie: Movie.first)
+end
+Bookmark.create!(user: User.last , movie: Movie.first)
 
 puts "Creating movie reviews"
-MovieReview.create!(
- user: User.first,
- movie: Movie.first,
- rating: 7,
- short_review: " J'ai bien aimé lol",
- directing_review: 'il touche le mec',
- acting_review: 'ils sont bons les acteurs',
- sound_review: 'Assassin de la police, woup woup!',
- story_review: "L'important, c'est l'aterrisage.",
- foreign_link: "https://police-nationale.com"
-)
+4.times do
+  MovieReview.create!(
+   user: User.first,
+   movie: Movie.first,
+   rating: 7,
+   short_review: " J'ai bien aimé lol",
+   directing_review: 'il touche le mec',
+   acting_review: 'ils sont bons les acteurs',
+   sound_review: 'Assassin de la police, woup woup!',
+   story_review: "L'important, c'est l'aterrisage.",
+   foreign_link: "https://police-nationale.com"
+  )
+end
 puts "Faking DB done ! 🌱"
