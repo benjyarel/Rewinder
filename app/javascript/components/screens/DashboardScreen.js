bf59c46a09bec09f8621efaeac390328d97bc406
Benjamin Leray <b.leray35@gmail.com>
2020-04-19 11:47:25 +0200

    app with router

---
+++ app/javascript/components/screens/DashboardScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DashboardScreen extends React.Component {
+  render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    );
+  }
+}
+
+export default DashboardScreen ;
