21734e5f723db90e89d8204b67cd5a231f3d60a6
Benjamin Leray <b.leray35@gmail.com>
2020-04-20 16:23:28 +0200

    redux setup, fetch movies, moviereviews and bookmarks action

--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import bookmarksReducer from './bookmarksReducer';
+import moviesReducer from './moviesReducer';
+import movieReviewsReducer from './movieReviewsReducer';
 
 export default combineReducers({
-  toto:  () => {return "toto"}
+  bookmarks: bookmarksReducer,
+  movie: moviesReducer,
+  movieReviews: movieReviewsReducer
 });
