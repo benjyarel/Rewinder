77eef18c3555325a44ecb56a4caa8d25c99e055e
Benjamin Leray <b.leray35@gmail.com>
2020-04-24 11:01:31 +0200

    use HashRouter, Bookmarks link

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { HashRouter, Switch, Route} from "react-router-dom";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from "../reducers"
 
 import Footer from './Footer';
-import DashboardScreen from './screens/DashboardScreen'
+
+import DashboardScreen from './screens/DashboardScreen';
+import BookmarksScreen from './screens/BookmarksScreen';
 import "./App.scss";
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers, applyMiddleware(thunk))} >
-        <BrowserRouter >
+        <HashRouter >
           <Switch>
             <Route path="/" exact component={DashboardScreen} />
+            <Route path="/bookmarks" exact component={BookmarksScreen} />
           </Switch>
-        </BrowserRouter>
-        <Footer />
+          <Footer />
+        </HashRouter>
       </Provider >
     );
   }
--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Footer.scss'
+import './Footer.scss';
+import { Link } from 'react-router-dom';
 
 
 class Footer extends React.Component {
@@ -7,6 +8,12 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <ul className="footer-buttons">
+          <Link to="/">
+            <li className="footer-button">
+              <i className="fas fa-home"></i>
+              <span>Home</span>
+            </li>
+          </Link>
           <li className="footer-button">
             <i className="fas fa-search"></i>
             <span>Recherche</span>
@@ -15,10 +22,12 @@ class Footer extends React.Component {
             <i className="fas fa-edit"></i>
             <span>Noter</span>
           </li>
-          <li className="footer-button">
-            <i className="fas fa-bookmark"></i>
-            <span>Favoris</span>
-          </li>
+          <Link to="/bookmarks">
+            <li className="footer-button">
+              <i className="fas fa-bookmark"></i>
+              <span>A voir</span>
+            </li>
+          </Link>
         </ul>
       </div>
     )
