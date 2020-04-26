7afb8aa62a7555804cb6f73415f1ab65c3f3678f
Benjamin Leray <b.leray35@gmail.com>
2020-04-26 12:26:40 +0200

    temp design for search

--- app/javascript/components/screens/SearchModal.js
+++ app/javascript/components/screens/SearchModal.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './SearchModal';
 
 const SearchModal = () => {
   return (
-    <div>
-      Search Modal
+    <div className="search-container">
+      <div className="search-movie-box">
+        <form className="search-movie-form">
+          <div className="form-group">
+            <label>Nom du film</label>
+            <input type="movie" className="form-control" id="" placeholder="Cherche un film..." />
+            <button type="submit" className="btn btn-warning ">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
